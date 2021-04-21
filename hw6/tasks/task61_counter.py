bd703c46624d4dcb6d56579b19057095d75527e3
Amudah41 <aleksey.erichev@gmail.com>
2021-04-21 15:39:06 +0300

    HW6

---
+++ hw6/tasks/task61_counter.py
@@ -0,0 +1,44 @@
+"""
+Написать декоратор instances_counter, который применяется к любому классу
+и добавляет ему 2 метода:
+get_created_instances - возвращает количество созданых экземпляров класса
+reset_instances_counter - сбросить счетчик экземпляров,
+возвращает значение до сброса
+Имя декоратора и методов не менять
+Ниже пример использования
+"""
+
+
+def instances_counter(cls):
+    class Wrapper(cls):
+        cls.count_of_obj = 0
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.__class__.count_of_obj += 1
+
+        @classmethod
+        def get_created_instances(cls):
+            return cls.count_of_obj
+
+        @classmethod
+        def reset_instances_counter(cls):
+            output = cls.count_of_obj
+            cls.count_of_obj = 0
+            return output
+
+    return Wrapper
+
+
+@instances_counter
+class User:
+    pass
+
+
+@instances_counter
+class OtherUser:
+    pass
+
+
+if __name__ == "__main__":
+    ...
