39286a6e4ac8e852fea8850bc3c94f7b1da125ed
Amudah41 <aleksey.erichev@gmail.com>
2021-04-30 00:16:43 +0300

    Try to use mock for test41

--- hw4/tests/task41_read_file_test.py
+++ hw4/tests/task41_read_file_test.py
@@ -13,25 +13,20 @@ from hw4.tasks.task41_read_file import (
 
 @pytest.mark.parametrize("number", [1, 2, 2.95])
 def test_is_useful_number_positive_test(number: int):
-    assert is_useful_number(number) == True
+    assert is_useful_number(number)
 
 
 @pytest.mark.parametrize("number", [0, 3, 0.95])
 def test_is_useful_number_negative_test(number: int):
-    assert is_useful_number(number) == False
+    assert not is_useful_number(number)
 
 
 def test_read_magic_number_only_value_error_positive_test():
-    assert (
-        read_magic_number_only_value_error("hw4/tests/task41_test_positive.txt") == True
-    )
+    assert read_magic_number_only_value_error("hw4/tests/task41_test_positive.txt")
 
 
 def test_read_magic_number_only_value_error_negative_test():
-    assert (
-        read_magic_number_only_value_error("hw4/tests/task41_test_negative.txt")
-        == False
-    )
+    assert not read_magic_number_only_value_error("hw4/tests/task41_test_negative.txt")
 
 
 def test_read_magic_number_only_value_error_value_error_test():
@@ -68,10 +63,33 @@ def test_file_producer(text, expected_result):
 
 #     monkeypatch.setattr(
 #         hw4.tasks.task41_read_file,
-#         "open",
+#         "builtin.open",
 #         mock
 #     )
 
 
 # def test_read_magic_number_all_exceptions_positive_test(my_path):
-#     assert read_magic_number_all_exceptions(my_path) == True
+#     assert read_magic_number_all_exceptions(my_path)
+
+
+# from unittest import TestCase
+# from unittest.mock import patch, Mock
+
+
+# class TestBlog(TestCase):
+#     @patch('main.Blog')
+#     def test_blog_posts(self, MockBlog):
+#         blog = MockBlog()
+
+#         blog.posts.return_value = [
+#             {
+#                 'userId': 1,
+#                 'id': 1,
+#                 'title': 'Test Title',
+#                 'body': 'Far out in the uncharted backwaters of the unfashionable end of the western spiral arm of the Galaxy\ lies a small unregarded yellow sun.'
+#             }
+#         ]
+
+#         response = blog.posts()
+#         self.assertIsNotNone(response)
+#         self.assertIsInstance(response[0], dict)
