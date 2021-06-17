604c72da1c78d18db65a9554767b5a8bbb190b12
Amudah41 <aleksey.erichev@gmail.com>
2021-06-17 17:11:39 +0300

    Working parser without treads.

--- hw10/tasks/parser.py
+++ hw10/tasks/parser.py
@@ -2,6 +2,7 @@
 # from concurrent.futures import ThreadPoolExecutor
 import json
 from datetime import datetime
+
 # from bs4.element import ProcessingInstruction
 import requests
 from bs4 import BeautifulSoup
@@ -19,33 +20,33 @@ def get_html(url):
 async def fetch_response(url):
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
-            return await response.text()
+            html = await response.text()
+
+            soup = BeautifulSoup(html, "lxml")
+            tds = soup.find("tbody", class_="table__tbody")
+            for link, growth in get_all_links_and_growth(tds):
+                await make_all(link, growth)
 
 
-def get_all_links_and_growth(html):
-    soup = BeautifulSoup(html, 'lxml')
-
-    tds = soup.find('tbody', class_='table__tbody')
-
-    while tds := tds.find_next('tr'):
-        a = tds.find(
-            'td', class_='table__td table__td--big').find('a').get('href')
-        link = 'https://markets.businessinsider.com' + a
-        growth = to_float(tds.find_all(
-            class_='table__td')[-1].find_all('span')[-1].text[:-1])
+def get_all_links_and_growth(tds):
+    while tds := tds.find_next("tr"):
+        a = tds.find("td", class_="table__td table__td--big").find("a").get("href")
+        link = "https://markets.businessinsider.com" + a
+        growth = to_float(
+            tds.find_all(class_="table__td")[-1].find_all("span")[-1].text[:-1]
+        )
 
         yield link, growth
 
 
 def actual_exchange_rate(html):
-    soup = BeautifulSoup(html, 'lxml')
-    return soup.find('valute', id="R01235").find('value').text
+    soup = BeautifulSoup(html, "lxml")
+    return soup.find("valute", id="R01235").find("value").text
 
 
 def to_float(number: str):
     try:
-        number = number.replace(",", '')
-        return float(number)
+        return float(number.replace(",", ""))
     except ValueError as err:
         raise err(f"Can't convert {number} to float.")
 
@@ -55,81 +56,134 @@ def convertion_USD_to_RUB(func):
 
         return func(to_float(price), USD)
 
-    USD = to_float(actual_exchange_rate(
-        get_html('http://www.cbr.ru/scripts/XML_daily.asp')))
+    USD = to_float(
+        actual_exchange_rate(get_html("http://www.cbr.ru/scripts/XML_daily.asp"))
+    )
 
     return wrapper
 
 
 @convertion_USD_to_RUB
 def convert(price, USD):
-    return price * USD
+    return round(price * USD, 2)
 
 
 async def get_page_data(html, growth):
-    soup = BeautifulSoup(html, 'lxml')
+    soup = BeautifulSoup(html, "lxml")
     lock = asyncio.Lock()
 
     # code_name_price_area = soup.find('div', class_="price-section__row")
 
-    code = soup.find('div', class_="price-section__row").find(
-        class_="price-section__category").find_next().text[2:]
+    code = (
+        soup.find("div", class_="price-section__row")
+        .find(class_="price-section__category")
+        .find_next()
+        .text[2:]
+    )
 
-    name = soup.find('div', class_="price-section__row").find(
-        class_="price-section__label").text[:-1]
+    if not bool(code):
+        print("\t some code STATUS: [NOT FOUND]")
+
+    name = (
+        soup.find("div", class_="price-section__row")
+        .find(class_="price-section__label")
+        .text[:-1]
+    )
+
+    print(name)
+
+    if not bool(name):
+        print("\t some name STATUS: [NOT FOUND]")
 
     async with lock:
         add_to_buffer(code, name, growth, "growth")
 
-    price = convert(soup.find('div', class_="price-section__row").find(
-        class_="price-section__current-value").text)
+    price = convert(
+        soup.find("div", class_="price-section__row")
+        .find(class_="price-section__current-value")
+        .text
+    )
+
+    if not bool(price):
+        print("\t price STATUS: [NOT FOUND]")
 
     async with lock:
-        add_to_buffer(code, name, price, 'price')
+        add_to_buffer(code, name, price, "price")
 
-    PE = to_float(soup.find(
-        'div', class_="snapshot__data-item").find(text=True, recursive=False))
+    PE = to_float(
+        soup.find("div", class_="snapshot__data-item").find(text=True, recursive=False)
+    )
+
+    if not bool(PE):
+        print("\t PE STATUS: [NOT FOUND]")
 
     async with lock:
-        add_to_buffer(code, name, -PE, 'PE')
+        add_to_buffer(code, name, -PE, "PE")
 
-    low_52_week = to_float(
-        soup.find(class_="snapshot__data-item snapshot__data-item--small").find(text=True, recursive=False))
+    try:
+        low_52_week = to_float(
+            soup.find(class_="snapshot__data-item snapshot__data-item--small").find(
+                text=True, recursive=False
+            )
+        )
+    except:
+        low_52_week = 1
+        print("\t low_52_week STATUS: [NOT FOUND]")
+    try:
 
-    high_52_week = to_float(soup.find(
-        class_="snapshot__data-item snapshot__data-item--small snapshot__data-item--right").find(text=True, recursive=False))
-    potential_profit = (high_52_week - low_52_week) * 100 / low_52_week
+        high_52_week = to_float(
+            soup.find(
+                class_="snapshot__data-item snapshot__data-item--small snapshot__data-item--right"
+            ).find(text=True, recursive=False)
+        )
+    except:
+        high_52_week = 1
+        print("\t high_52_week STATUS: [NOT FOUND]")
+
+    potential_profit = round((high_52_week - low_52_week) * 100 / low_52_week, 2)
 
     async with lock:
         add_to_buffer(code, name, potential_profit, "potential_profit")
 
 
 def write_json(file, data):
-    with open(file, 'a') as f:
+    with open(file, "a") as f:
         json.dump(data, f, indent=4)
-        print(f'{file} completed!')
+        print(f"{file} completed!")
 
 
 async def make_all(link, growth):
-    html = await fetch_response(link)
-    # response = await fetch_response(link)
-    # print(response)
-    # html = await response.text()
-    # print(html)
-    await get_page_data(html, growth)  # нужна ли здесь ассинхроность - ???
+    async with aiohttp.ClientSession() as session:
+        async with session.get(link) as response:
+            html = await response.text()
+            # response = await fetch_response(link)
+            # print(response)
+            # html = await response.text()
+            # print(html)
+            await get_page_data(html, growth)  # нужна ли здесь ассинхроность - ???
 
     # write_json(data)
 
 
-def write_results(func, top_10_price=[], top_10_PE=[],
-                  top_10_growth=[], top_10_potential_profit=[], write_to_file=False):
+def write_results(
+    func,
+    top_10_price=[],
+    top_10_PE=[],
+    top_10_growth=[],
+    top_10_potential_profit=[],
+    write_to_file=False,
+):
 
-    tops = {'price': top_10_price, "PE": top_10_PE,
-            "growth": top_10_growth, "potential_profit": top_10_potential_profit}
+    tops = {
+        "price": top_10_price,
+        "PE": top_10_PE,
+        "growth": top_10_growth,
+        "potential_profit": top_10_potential_profit,
+    }
 
     if write_to_file:
         for key, value in tops.items():
-            write_json('Top_10_' + key + '.json', value)
+            write_json("Top_10_" + key + ".json", value)
 
     def wrapper(code, name, param, param_name):
         return func(code, name, param, tops[param_name])
@@ -171,7 +225,7 @@ def pass_function():
 
 async def main():
     start = datetime.now()
-    url = 'https://markets.businessinsider.com/index/components/s&p_500'
+    url = "https://markets.businessinsider.com/index/components/s&p_500?p="
 
     # with open('index.html') as f:
     #     html = f.read()
@@ -182,8 +236,15 @@ async def main():
     # all_companies = [[link, growth]
     #                  for link, growth in get_all_links_and_growth(html)]
 
-    tasks = [asyncio.create_task(make_all(link, growth))
-             for link, growth in get_all_links_and_growth(get_html(url))]
+    tasks = [
+        asyncio.create_task(fetch_response(url + str(page))) for page in range(1, 11)
+    ]
+
+    # async for link, growth in get_all_links_and_growth(fetch_response(url + str(page))):
+    #     tasks.append(asyncio.create_task(make_all(link, growth)))
+
+    # event_loop = asyncio.get_event_loop()
+    # event_loop.run_until_complete(get_sp500())
 
     await asyncio.gather(*tasks)
 
@@ -194,6 +255,10 @@ async def main():
     print(str(total))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     asyncio.run(main())
-    # 0:00:16.436560
+    # 0:03:36.260163
+
+# American Electric Power Co. Inc.
+# Honeywell
+# Host Hotels & Resorts Inc.
